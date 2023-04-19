9dc82ce4a6b52ace89e913c476bbcc992d244843
dotnet-code <@dotnet-code>
2023-04-19 17:30:54 +0530

    commit from code202304193054

---
+++ test_a_15/Controllers/GoogleDriveController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Test_A_15;
+
+namespace Test_A_15.Controllers
+{
+    public class GoogleDriveController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult ConfigureGoogleDrive(string DriveClientId, string DriveClientSecret, string DriveRefreshToken)
+        {
+            try
+            {
+                // Configure your Google Drive settings
+                // ...
+
+                return Ok("Google Drive configured successfully");
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
